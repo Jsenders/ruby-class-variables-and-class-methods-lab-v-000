19d62a4e5d7fd31e3bf4300517f91c7aa580d0cd
Joseph Senders <jasenders@gmail.com>
2018-08-16 14:46:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,4 +15,20 @@ def initialize(name, artist, genre)
 	@@artists << artist
 end
 
+def self.count 
+  @@count 
+end
+
+def self.genres 
+end
+
+def self.artists
+end
+
+def self.genre_count 
+end
+
+def self.artist_count
+end
+
 end
